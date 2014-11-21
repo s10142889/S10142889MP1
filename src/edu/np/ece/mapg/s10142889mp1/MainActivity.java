468ccf70924928a74d0ef39a7c83b8e13208cb63
s10142889 <s10142889@connect.np.edu.sg>
2014-11-21 13:49:19 -0800

    Modification to mainactivity
    This is simple description.

--- src/edu/np/ece/mapg/s10142889mp1/MainActivity.java
+++ src/edu/np/ece/mapg/s10142889mp1/MainActivity.java
@@ -4,16 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	EditText etMessage;
+	Button btGuess;
 	int myNumber=9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etMessage=(EditText)this.findViewById(R.id.etNumber);
+        btGuess=(Button)this.findViewById(R.id.btGuess);
+        btGuess.setOnClickListener(Listener);
+        
     }
-
+    private View.OnClickListener Listener=new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			String str=etMessage.getText().toString();
+			int num=Integer.parseInt(str);
+			if(num==myNumber){
+				Toast.makeText(MainActivity.this, "Bingo! Correct number.", Toast.LENGTH_SHORT).show();
+			}
+			else{
+				Toast.makeText(MainActivity.this, "Try again!", Toast.LENGTH_SHORT).show();
+			}
+			// TODO Auto-generated method stub
+			
+		}
+	};
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
